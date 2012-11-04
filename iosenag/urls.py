0ecbcd4eedd2892a29cbc386aa6d51e7a709aeb2
IoseNag <web@iosenag.net>
2012-11-04 11:26:44 +0100

    Add a 404 and a 500 pages

--- iosenag/urls.py
+++ iosenag/urls.py
@@ -5,5 +5,7 @@ import mirror.urls
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    (r'^404', TemplateView.as_view(template_name='404.html')),
+    (r'^500', TemplateView.as_view(template_name='500.html')),
     url(r'^mirror/', include(mirror.urls, namespace='mirror')),
 )
