c916c8193036c927ba245bb355b9ce2dd4560b54
IoseNag <web@iosenag.net>
2013-01-19 14:56:19 +0100

    Only activate admin if settings.DEBUG is True

--- iosenag/urls.py
+++ iosenag/urls.py
@@ -1,18 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from iosenag import settings
 import mirror.urls
 import search.urls
-
-# Uncomment the next two lines to enable the admin:
-#from django.contrib import admin
-#admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     (r'^404', TemplateView.as_view(template_name='404.html')),
     (r'^500', TemplateView.as_view(template_name='500.html')),
-    url(r'^mirror/', include(mirror.urls, namespace='mirror')),
-    url(r'^search/', include(search.urls, namespace='search')),
-    # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
+    (r'^mirror/', include(mirror.urls, namespace='mirror')),
+    (r'^search/', include(search.urls, namespace='search')),
 )
+
+# Only enable the admin in development mode, not in production
+if settings.DEBUG:
+    from django.contrib import admin
+    admin.autodiscover()
+    urlpatterns.append(url(r'^admin/', include(admin.site.urls)))
