64fd1c8c15df18c1d0b12793d32bfb2d24a7ec57
IoseNag <web@iosenag.net>
2017-09-13 21:23:20 +0200

    django.conf.urls.patterns has been removed

--- iosenag/urls.py
+++ iosenag/urls.py
@@ -1,18 +1,18 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import RedirectView, TemplateView
 import iosenag.mirror.urls
 import iosenag.search.urls
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    ('^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'icons/favicon.ico')),
-    ('^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-    (r'^404/?$', TemplateView.as_view(template_name='404.html')),
-    (r'^500/?$', TemplateView.as_view(template_name='500.html')),
-    (r'^mirror/', include(iosenag.mirror.urls, namespace='mirror')),
-    (r'^search/', include(iosenag.search.urls, namespace='search')),
-)
+    url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'icons/favicon.ico')),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^404/?$', TemplateView.as_view(template_name='404.html')),
+    url(r'^500/?$', TemplateView.as_view(template_name='500.html')),
+    url(r'^mirror/', include(iosenag.mirror.urls, namespace='mirror')),
+    url(r'^search/', include(iosenag.search.urls, namespace='search')),
+]
 
 # Only enable admin urls if admin app is loaded
 if 'django.contrib.admin' in settings.INSTALLED_APPS:
